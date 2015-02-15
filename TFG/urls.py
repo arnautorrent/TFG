7ba0acbc5f2d4738ad3bdff3f27816abb7e6bfae
arnau <nauix26@gmail.com>
2015-02-15 20:18:10 +0100

    Setting up URL's

--- TFG/urls.py
+++ TFG/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'TFG.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
+    url(r'^tfg/', include('Recommender.urls', namespace="Recommender")),
     url(r'^admin/', include(admin.site.urls)),
 )
