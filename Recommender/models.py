eccc9a6bf765dafb2ff144385d11f36098f38dd0
arnau <nauix26@gmail.com>
2015-02-25 00:34:18 +0100

    Segona versió del formulari de Data Entry (amb Ajax i primeres dades de la BDD).

--- Recommender/models.py
+++ Recommender/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class MusicGenre(models.Model):
+    genre = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.genre
--- Recommender/views/DataEntry.py
+++ Recommender/views/DataEntry.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from Recommender.models import MusicGenre
 
 def DataEntry(request):
     template_name = 'Recommender/data_entry.html'
-    return render(request,template_name)
+    genre_list = MusicGenre.objects.all()
+    context = {'genre_list':genre_list }
+    return render(request,template_name, context)
