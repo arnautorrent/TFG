38f56712864c46e02104971d17a3809c79521a0c
arnau <nauix26@gmail.com>
2015-04-22 01:57:57 +0200

    (minor edit) - Main Algorithm scheme

--- Recommender/views/MainAlgorithm.py
+++ Recommender/views/MainAlgorithm.py
@@ -8,42 +8,49 @@ from pyechonest import config, artist, song
 
 
 def MainAlgorithm(request):
-    #TODO Calcular la llista
-    time.sleep(10)
-
-
-
-
-    #ADAPTACIÓ ALGORITME GUILLEM
+    #TODO ADAPTACIÓ ALGORITME GUILLEM
 
     config.ECHO_NEST_API_KEY = Constants.ECHONEST_API_KEY #Poso la API key
-    playlist_length = 18
+    playlist_length = 20
     percentage_of_direct_musical_data = 0.50
 
-    #===============================LOAD USER INFORMATION================================
-    # He de mirar l'ID, i agafar les dades que toquin de la BDD.
-    # DADES:
+    #===========================LOAD USER $ FORM INFORMATION=============================
+    #
+    # He de mirar l'ID, i agafar les dades que toquin de la BDD:
     #   - Nom
-    #   - Email ?
     #   - Data de naixement
     #   - LLoc de naixement
     #   - LLocs on ha viscut
     #   - Gèneres preferits
+    #   - Cançons preferides
+    #   - Artistes preferits
     #
+    # LA PLAYLIST ÉS UN VECTOR AMB ID'S DE CANÇONS DE LA BDD???
     #
-
-
-    #=======================================================
-
-
-
-
-
-
-
-
+    #==============================FAVOURITE SONGS=======================================
+    #
+    # 1) Fem una llista amb les cançons preferides (aquestes hi van segur).
+    #
+    #==================================ARTISTS===========================================
+    #
+    # 1) Fem una llista amb els artistes preferits.
+    # 2) Afegim els artistes de les cançons preferides.
+    # 3) Fem una llista d'artistes similars.
+    # 4) Fem una llista de "TOP SONGS" dels artistes similars.
+    #
+    #============================PREFERENCE FILTER 1=====================================
+    #
+    # És la llista amb cançons preferides i cançons TOP dels artistes preferits
+    # i relacionats fins a arribar al percentage_of_direct_musical_data.
+    #
+    #============================PREFERENCE FILTER 2=====================================
+    #
+    # Aquí farem cerques de possibles cançons per regió/localització, tempo, ballable,
+    # estil, any, ... [[AQUÍ ÉS ON HI HA LA XIXA DE MILLORA DE L'ALGORITME]].
+    #
+    #====================================================================================
 
     # RENDER la playlist
     template_name = 'Recommender/results.html'
-    context = {'firstName':'playlist'}
+    context = {'playlist':'playlist'}
     return render(request,template_name, context)
